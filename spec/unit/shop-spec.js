490e6be2f4187c824fad03dfd02bd9723827de2f
shektor <44544977+shektor@users.noreply.github.com>
2019-06-07 11:27:41 +0100

    Add unit test for item sulfuras
    Item does not follow normal behaviour and does not increase or decrease
    in both sellIn and quality when updateQuality is run.

--- spec/unit/shop-spec.js
+++ spec/unit/shop-spec.js
@@ -97,5 +97,33 @@ describe('Shop', () => {
 
       expect(items).toEqual([itemAfter]);
     });
+
+    it('sulfras does not decrease sellIn or in quality with negative sellIn', () => {
+      itemBefore.name = 'Sulfuras, Hand of Ragnaros';
+      itemBefore.sellIn = -1;
+      itemBefore.quality = 5;
+
+      itemAfter.name = 'Sulfuras, Hand of Ragnaros';
+      itemAfter.sellIn = -1;
+      itemAfter.quality = 5;
+
+      const items = gildedRose.updateQuality();
+
+      expect(items).toEqual([itemAfter]);
+    });
+
+    it('sulfras does not decrease sellIn or in quality with positive sellIn', () => {
+      itemBefore.name = 'Sulfuras, Hand of Ragnaros';
+      itemBefore.sellIn = 1;
+      itemBefore.quality = 50;
+
+      itemAfter.name = 'Sulfuras, Hand of Ragnaros';
+      itemAfter.sellIn = 1;
+      itemAfter.quality = 50;
+
+      const items = gildedRose.updateQuality();
+
+      expect(items).toEqual([itemAfter]);
+    });
   });
 });
